b054618e30712564f1ff6ebb7e5440cd53c59df7
jecrell <matt.walls31@gmail.com>
2018-06-05 16:11:50 +0900

    Initial commit

---
+++ Source/RemoveModernStuff/ModStuff.cs
@@ -0,0 +1,24 @@
+﻿using Harmony;
+using RimWorld;
+using Verse;
+
+namespace TheThirdAge
+{
+    public class ModStuff : Mod
+    {
+        public ModStuff(ModContentPack content) : base(content)
+        {
+                HarmonyInstance harmony = HarmonyInstance.Create("rimworld.lotr.thirdageunlimited");
+
+                harmony.Patch(AccessTools.Method(typeof(DefGenerator), "GenerateImpliedDefs_PreResolve"), null,
+                new HarmonyMethod(typeof(ModStuff), nameof(GenerateImpliedDefs_PreResolve)), null);
+         
+         
+        }
+        
+        public static void GenerateImpliedDefs_PreResolve()
+        {
+            OnStartup.AddSaltedMeats();
+        }
+    }
+}
